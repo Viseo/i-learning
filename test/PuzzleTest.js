6754224024cd6de04bb9d712115a63c67b522e7c
Dupont <TDU3482@viseo.net>
2016-02-29 16:32:46 +0100

    Tests added (on functions initTiles and display)

--- test/PuzzleTest.js
+++ test/PuzzleTest.js
@@ -21,16 +21,36 @@ describe('Puzzle test suite', function() {
 
         expect(puzzle.lines).toEqual(2);
         expect(puzzle.rows).toEqual(3);
-        expect(puzzle.questionsTab).toEqual(questions);// le toEqual ne marche peut-être pas ici (comparaison d'adresse)
+        expect(puzzle.questionsTab).toEqual(questions);
 
     });
 
-    it('should initiate the array of tiles',function(){
+    it('should initialize the array of tiles',function(){
+
+        expect(puzzle).toBeDefined();
+
         puzzle.paper=Raphael(0,0,200,200);
+
+        expect(puzzle.tileWidth).toBeUndefined();
+        expect(puzzle.tileHeight).toBeUndefined();
+
         puzzle.initTiles();
 
         expect(puzzle.tileWidth).toBeDefined();
         expect(puzzle.tileHeight).toBeDefined();
+        expect(puzzle.tilesTab.length).toEqual(questions.length);
+        var truc=puzzle.tilesTab[4].text.attr('text');
+        console.log(truc);
+        expect(puzzle.tilesTab[4].text.attr('text')).toEqual(questions[4].label);
+
+    });
+
+    it('should initialize the main svg element/tag',function(){
+        puzzle.display(120,150,800,600);
+
+        expect(puzzle.paper).toBeDefined();
+        expect (puzzle.paper.height).toEqual(600);
+        expect (puzzle.paper.width).toEqual(800);
 
     });
 
