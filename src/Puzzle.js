43121c471b9ffa63ed6221b104430c82921b2710
AlexisBlanchardViseo <alexis.blanchard@viseo.com>
2016-02-29 16:41:43 +0100

    changes: tiles position in initTiles

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -22,14 +22,28 @@ function Puzzle(lines, rows,questionsTab)
     {
         self.tileWidth=(self.paper.width-(self.rows+1)*self.margin)/self.rows;
         self.tileHeight=(self.paper.height-(self.lines+1)*self.margin)/self.lines;
+
+        posy=self.margin;
+        posx=self.margin;
+        var count=0;
         for(var i=0; i<questionsTab.length;i++)
         {
             var newTile;
-            var posx,posy;
-            posx=self.margin+i*(self.tileWidth+self.margin);
-            posy=self.margin+i*(self.tileHeight+self.margin);
-            var R=self.paper.rect(posx,posy,self.tileWidth,self.tileHeight);
-            var T=self.paper.text(posx+self.tileWidth/2,posy+self.tileHeight/2,questionsTab[i].label);
+            var posx,posy, R, T, x, y;
+            if(i!==0)
+            {
+                posx+=(self.tileWidth+self.margin);
+            }
+            if(count>(self.rows-1))
+            {
+                count=0;
+                posy+=(self.tileHeight+self.margin);
+                posx=self.margin;
+            }
+            count++;
+
+            R=self.paper.rect(posx,posy,self.tileWidth,self.tileHeight);
+            T=self.paper.text(posx+self.tileWidth/2,posy+self.tileHeight/2,questionsTab[i].label);
             newTile={rect:R,text:T};
             self.tilesTab.push(newTile);
         }
