4372c98a25a100263cafa2b7b35796328c9827ea
AlexisBlanchardViseo <alexis.blanchard@viseo.com>
2016-02-29 14:30:22 +0100

    bug fix, initTiles

--- src/Puzzle.js
+++ src/Puzzle.js
@@ -24,13 +24,13 @@ function Puzzle(lines, rows,questionsTab)
         self.tileHeight=(self.paper.height-(self.lines+1)*self.margin)/self.lines;
         for(var i=0; i<questionsTab.length;i++)
         {
-            var newTile=[];
+            var newTile;
             var posx,posy;
             posx=self.margin+i*(self.tileWidth+self.margin);
             posy=self.margin+i*(self.tileHeight+self.margin);
             var R=self.paper.rect(posx,posy,self.tileWidth,self.tileHeight);
             var T=self.paper.text(posx+self.tileWidth/2,posy+self.tileHeight/2,questionsTab[i].label);
-            newTile.push({rect:R,text:T});
+            newTile={rect:R,text:T};
             self.tilesTab.push(newTile);
         }
     }
