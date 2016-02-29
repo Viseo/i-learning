757a11f32e7ca90a95a02b610f454f4a2a514de2
AlexisBlanchardViseo <alexis.blanchard@viseo.com>
2016-02-29 11:23:16 +0100

    puzzle constructor,init tiles

---
+++ src/Puzzle.js
@@ -0,0 +1,38 @@
+/**
+ * Created by ABL3483 on 29/02/2016.
+ */
+
+function Puzzle(lines, rows,questionsTab)
+{
+    var self=this;
+    self.lines=lines;
+    self.rows=rows;
+    self.paper = Raphael("puzzle", 200, 75);
+    self.margin=15;
+    self.tileWidth;
+    self.tileHeight;
+    self.tilesTab=[];
+    self.questionsTab=questionsTab;
+    self.display=function()
+    {
+        initTiles();
+        
+    };
+
+    function initTiles()
+    {
+        self.tileWidth=(self.paper.width-(self.rows+1)*self.margin)/self.rows;
+        self.tileHeight=(self.paper.height-(self.lines+1)*self.margin)/self.lines;
+        for(var i=0; i<questionsTab.length;i++)
+        {
+            var newTile;
+            var posx,posy;
+            posx=self.margin+i*(self.tileWidth+self.margin);
+            posy=self.margin+i*(self.tileHeight+self.margin);
+            var R=self.paper.rect(posx,posy,self.tileWidth,self.tileHeight);
+            var T=self.paper.text(posx+self.tileWidth/2,posy+self.tileHeight/2,questionsTab[i].label);
+            newTile.push({rect:R,text:T});
+            self.tilesTab.push(newTile);
+        }
+    }
+}
