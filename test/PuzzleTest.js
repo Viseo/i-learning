ab6e511df2c2ee39a5e88c202fe37008402876dc
Dupont <TDU3482@viseo.net>
2016-03-11 15:03:21 +0100

    Modified puzzleTest (should be passing, 1 test)

--- test/PuzzleTest.js
+++ test/PuzzleTest.js
@@ -23,7 +23,7 @@ describe('Puzzle test suite', function() {
             questions.pop();
         }
 
-        var puzzle=new Puzzle(2,3,questions,quizz.cadreResult);
+        var puzzle=new Puzzle(2,2,questions,quizz.cadreResult);
 
         expect(puzzle).toBeDefined();
 
@@ -37,33 +37,13 @@ describe('Puzzle test suite', function() {
 
         puzzle.display(quizz.cadreResult.x, quizz.cadreResult.y+quizz.cadreResult.h+15, quizz.cadreResult.w, 600, 0);
 
-        console.log(puzzle.displaySet[2]);
+        var q=puzzle.displaySet[1];
+       //console.log(puzzle.displaySet.length);
 
-        expect(puzzle.displaySet[2].attr('text')).toEqual(questions[0].label);
+        console.log(q.attr('text'));
+        console.log(questions[0].label);
+        expect(q.attr('text')).toEqual(questions[0].label);
 
-    });
-
-    it('should check the right values for this example (auto generated)',function() {
-        // puzzle.paper.writeTest();
-        //
-        puzzle= new Puzzle(0,0,1500,1500,2,4,[{label:'Q1'},{label:'Q2'},{label:'Q3'},{label:'Q4'},{label:'Q5'},{label:'Q6'},{label:'Q7'},{label:'Q8'}]);
-        puzzle.display();
-        paper.r0.test(15,15,356.25,727.5);
-        paper.t1.test(193.125,378.75,"Q1");
-        paper.r2.test(386.25,15,356.25,727.5);
-        paper.t3.test(564.375,378.75,"Q2");
-        paper.r4.test(757.5,15,356.25,727.5);
-        paper.t5.test(935.625,378.75,"Q3");
-        paper.r6.test(1128.75,15,356.25,727.5);
-        paper.t7.test(1306.875,378.75,"Q4");
-        paper.r8.test(15,757.5,356.25,727.5);
-        paper.t9.test(193.125,1121.25,"Q5");
-        paper.r10.test(386.25,757.5,356.25,727.5);
-        paper.t11.test(564.375,1121.25,"Q6");
-        paper.r12.test(757.5,757.5,356.25,727.5);
-        paper.t13.test(935.625,1121.25,"Q7");
-        paper.r14.test(1128.75,757.5,356.25,727.5);
-        paper.t15.test(1306.875,1121.25,"Q8");
     });
 
 });
