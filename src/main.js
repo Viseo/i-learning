08156b4092aac0ffb87f18ca16f250b10756243f
Dupont <TDU3482@viseo.net>
2016-04-01 14:32:06 +0200

    Cleaning main.js

--- src/main.js
+++ src/main.js
@@ -8,6 +8,9 @@ var drawings = new Drawings(document.body.clientWidth, document.documentElement.
 var drawing = drawings.drawing;
 var mainManipulator = drawing.manipulator;
 
+
+//mainManipulator.translator.move(document.body.clientWidth/4, document.documentElement.clientHeight/4);
+
 function main() {
 
   /*  myQuizz.tabQuestions[0].tabAnswer[0].bCorrect=true;
@@ -30,92 +33,28 @@ function main() {
     //    quizz.displaySet.getTarget(0,0);
     //},2000);
 
-    //displaySet.height= window.innerHeight;
-    //displaySet.width = window.innerWidth;
-    //viewBoxWidth = window.innerWidth;
-    //viewBoxHeight = window.innerHeight;
 
     function resizePaper(){
-        var w = 0, h = 0;
-        if (window.innerWidth){
-            w = window.innerWidth;
-            h = window.innerHeight;
-        }else if(document.documentElement &&
-            (document.documentElement.clientWidth ||
-            document.documentElement.clientHeight)) {
-            w = document.documentElement.clientWidth;
-            h = document.documentElement.clientHeight;
-        }
+
         var scale =1;
-        /*if (displaySet.height!=h && displaySet.width==w){
-            scale = h/displaySet.height;
-        }
-        else if (displaySet.height==h && displaySet.width!=w) {
-            scale = w/displaySet.width;
-            console.log("ici");
-        }
-        else {
-            scale = 1/Math.max(displaySet.height/h, displaySet.width/w);
+        var scaleY=window.oldHeight/window.innerHeight;
+        window.oldHeight=window.innerHeight;
+        var scaleX=window.oldWidth/window.innerWidth;
+        window.oldWidth=window.innerWidth;
 
-        }*/
-        if (viewBoxHeight!=h && viewBoxWidth==w){
-            scale = h/viewBoxHeight;
-        }
-        else if (viewBoxHeight==h && viewBoxWidth!=w) {
-            scale = w/viewBoxWidth;
-            console.log("ici");
-        }
-        else {
-            scale = 1/Math.max(viewBoxHeight/h, viewBoxWidth/w);
 
-        }
-        /*if(scale<1){
-            scale*=0.95;
-        }
-        else {
-            scale*=1.05;
-        }*/
-        //scale = 1/Math.max(displaySet.height/h, displaySet.width/w);
-        displaySet.height*=scale;
-        displaySet.width*=scale;
-        viewBoxHeight/=scale;
-        viewBoxWidth/=scale;
-        //displaySet.scale(scale);
-        //viewBoxWidth
+        scale=Math.min(scaleX,scaleY);
 
-        //paper.setViewBox(0, 0, document.body.clientWidth*scale/2, 1500*scale/2, false);
-       // paper.setViewBox(0, 0, viewBoxHeight, viewBoxWidth, false);
+        //quizz.tabQuestions[0].tabAnswer[1].answerManipulator.scalor.scale(scale);
+        quizz.quizzManipulator.scalor.scale(scale);
+        quizz.quizzManipulator.translator.move((document.body.clientWidth/2)*scale,(document.documentElement.clientHeight/2)*scale);
         console.log(scale);
-        //displaySet.scale(1/window.devicePixelRatio);
-        //console.log(scale);
-        //window.devicePixelRatio = 1;
-        //console.log("zoom" + window.devicePixelRatio);
-        /*displaySet[0].forEach(function(element){
-            element.scale(scale);
-        });*/
+
     }
-    //window.onresize = function(){
-    //    resizePaper();
-    //}
+    window.oldWidth=window.innerWidth;
+    window.oldHeight=window.innerHeight;
+
+    window.onresize = resizePaper;
 
 
-
-    /*setTimeout(function(){
-        quizz.tabQuestions[0].displaySet.transform('...t50,50');
-    },500);*/
-    // Navigation Puzzle
-    /*var puzzle = new Puzzle(3, 3, tabQuestions);
-     puzzle.display(20, 20, 600, 600, 0);*/
-
-    //var quizz=new Quizz("Ceci est le titre du Quizz n°1",tabQuestions);
-    //quizz.display();
-
-    // Display in rows
-    /*var nbRows = 3;
-     var tabAnswer = [{label: "", imageSrc: null, bCorrect: false}, {label: "", imageSrc: null, bCorrect: false}, {label: "", imageSrc: null, bCorrect: false}, {label: "", imageSrc: null, bCorrect: false}, {label: "", imageSrc: null, bCorrect: false}];
-     var question = new Question(null, null, tabAnswer, nbRows);
-     question.display(50, 50, 1000, 200);*/
-
-    //Display Resultat Puzzle
-    //quizz.displayResult(50, 10, 1200, 1200, myColors.grey);
 }
