4f26d7c6fe0af59c611654fd40627357e39fb109
agcaroff <anne.caroff@telecom-bretagne.eu>
2016-04-14 17:46:33 +0200

    entete formationsManager

--- src/FormationsManager.js
+++ src/FormationsManager.js
@@ -5,45 +5,38 @@
 function FormationsManager() {
     var self = this;
     //self.ilearningBandeau = new ??
-    self.x = document.body.clientWidth/15; //??
-    self.y = 50; //self.ilearningBandeau.height
-    self.addButtonWidth = document.body.clientWidth/4;
-    self.addButtonHeight = document.body.clientHeight/20;
+    //self.x = document.body.clientWidth/15; //??
+    self.x = 10;
+    self.y = 20; //self.ilearningBandeau.height
+    self.addButtonWidth = 330;
+    self.addButtonHeight = 40;
     self.fontSize = 20;
     self.plusDim = self.fontSize*2;
 
     self.formationsManagerManipulator = new Manipulator();
-    self.formationsManagerManipulator.translator.move(self.x, self.y);
     mainManipulator.ordonator.set(0,self.formationsManagerManipulator.first);
-    //self.title = new svg.Text("Formations");
-    //self.formationsManagerManipulator.last.add(self.title);
-    //self.title.move
     self.title = autoAdjustText("Formations", 0, 0, self.addButtonWidth, self.addButtonHeight, self.fontSize, null, self.formationsManagerManipulator);
-    //self.title.text.position(self.x, 0);
+    self.title.text.position(-self.addButtonWidth/4,0);
 
     self.addButtonManipulator = new Manipulator();
     self.formationsManagerManipulator.last.add(self.addButtonManipulator.first);
     self.addButtonManipulator.translator.move(self.plusDim/2, self.addButtonHeight);
     self.addFormationButton = autoAdjustText("Ajouter une formation", -self.addButtonWidth/2, self.addButtonHeight/2, self.addButtonWidth, self.addButtonHeight, self.fontSize, null, self.addButtonManipulator);
-    //self.addFormationButton = displayTextWithoutCorners("Ajouter une formation", self.addButtonWidth, self.addButtonHeight, myColors.none, myColors.lightgrey, self.fontSize, null, self.addButtonManipulator);
-    self.addFormationButton.text.position(self.plusDim + self.x/2,self.addFormationButton.text.y);
+    self.addFormationButton.text.position(self.x/2,self.addFormationButton.text.y);
+    self.formationsManagerManipulator.translator.move(self.addButtonWidth/2, self.y);
+
 
     self.addFormationCadre = new svg.Rect(self.addButtonWidth, self.addButtonHeight).color(myColors.lightgrey).position(self.x,0);
     self.addButtonManipulator.ordonator.set(0,self.addFormationCadre);
 
     self.imageSrc = "../resource/plusSign.svg.png";
-    self.image = imageController.getImage(self.imageSrc, function () {
-        self.imageLoaded = true;
-        console.log(self.image);
-        self.dimImage = {width:self.image.width, height:self.image.height};
-    });
 
     self.dimImage = {width:self.plusDim, height:self.plusDim};
-    console.log(self.dimImage);
 
     self.addFormationObject = displayImage(self.imageSrc, self.dimImage, self.plusDim, self.plusDim).image;
-    self.addButtonManipulator.ordonator.set(2,self.addFormationObject);
-    self.addFormationObject.position(-self.plusDim,0);
+    self.addButtonManipulator.ordonator.set(2, self.addFormationObject);
+    self.addFormationObject.position(-self.addButtonWidth/2+self.plusDim, 0);
+
 
     function onClickAddFormation(){
         console.log("Tu as bien cliqué");
@@ -56,9 +49,26 @@ function FormationsManager() {
 
     self.legendManipulator = new Manipulator();
     self.formationsManagerManipulator.last.add(self.legendManipulator.first);
-    self.legendManipulator.translator.move(document.body.clientWidth/2);
+    self.legendManipulator.translator.move(document.body.clientWidth/1.5, 0);
+    self.legendDim = self.plusDim/2;
+
+    self.checkManipulator = new Manipulator();
+    self.legendManipulator.last.add(self.checkManipulator.first);
     self.checkLegendSrc = "../resource/check.png";
-    self.checkLegend = displayImage(self.checkLegendSrc, self.dimImage, self.plusDim, self.plusDim).image;
+    self.checkLegend = displayImage(self.checkLegendSrc, self.dimImage, self.legendDim, self.legendDim).image;
+    self.legendManipulator.ordonator.set(2,self.checkLegend);
+    self.published = autoAdjustText("Publié", 0, 0, self.addButtonWidth, self.addButtonHeight, self.fontSize/1.5, null, self.checkManipulator);
+    self.checkManipulator.translator.move(self.published.text.component.getBBox().width,0);
+    self.published.text.position(0, self.published.text.y);
+
+    self.exclamationManipulator = new Manipulator();
+    self.checkManipulator.last.add(self.exclamationManipulator.first);
+    self.exclamationLegendSrc = "../resource/exclamation.png";
+    self.exclamationLegend = displayImage(self.exclamationLegendSrc, self.dimImage, self.legendDim, self.legendDim).image;
+    self.exclamationManipulator.ordonator.set(3,self.exclamationLegend);
+    self.exclamationManipulator.translator.move(document.body.clientWidth/12, 0);
+    self.toPublish = autoAdjustText("Nouvelle version à publier", 0, 0, self.addButtonWidth, self.addButtonHeight, self.fontSize/1.5, null, self.exclamationManipulator);
+    self.toPublish.text.position(self.toPublish.text.component.getBBox().width/2+self.legendDim, self.toPublish.text.y);
 
 
 };
