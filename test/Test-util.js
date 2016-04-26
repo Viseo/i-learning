218a5bbc4e8141f155125c2275b290ed7d77c2d7
Dupont <TDU3482@viseo.net>
2016-04-26 16:32:11 +0200

    Re added accidentally deleted functions in Test-util

--- test/Test-util.js
+++ test/Test-util.js
@@ -25,7 +25,16 @@ var ImageRuntime = {
     }
 };
 
-
+var ImageController = function (imageRuntime) {
+    return imageRuntime || {
+        getImage: function (imgUrl, onloadHandler) {
+            var image = new Image();
+            image.src = imgUrl;
+            image.onload = onloadHandler;
+            return image;
+        }
+    };
+};
 
 var onClickMock = function (obj, x, y) {
     obj.node.onclick({clientX:x, clientY:y});
@@ -66,3 +75,16 @@ var AsyncTimerRuntime = {
     }
 };
 
+var AsyncTimerController = function (asyncTimerRuntime) {
+    return asyncTimerRuntime || {
+        interval: function (handler, timer) {
+            return setInterval(handler, timer);
+        },
+        clearInterval: function (id) {
+            clearInterval(id);
+        },
+        timeout: function (handler, timer) {
+            return setTimeout(handler, timer);
+        }
+    };
+};
