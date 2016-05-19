037704dcb588cea2ac582712f689a9d5fd6690a9
agcaroff <anne.caroff@telecom-bretagne.eu>
2016-05-19 16:00:28 +0200

    Bug fix de l'allegement

--- src/GUI.js
+++ src/GUI.js
@@ -280,7 +280,8 @@ function LibraryDisplay(x,y,w,h){
                 self.draggedObjectLabel = "";
             };
             svg.event(drawings.glass,"mousedown",event);
-            img.component.listeners ? svg.removeEvent(img, 'mouseup', img.component.listeners.mouseup):svg.removeEvent(img, 'mouseup', img.component.target.listeners.mouseup);
+            img.component.listeners && svg.removeEvent(img, 'mouseup', img.component.listeners.mouseup);
+            img.component.target.listeners && img.component.target.listeners.mouseup && svg.removeEvent(img, 'mouseup', img.component.target.listeners.mouseup);
             svg.addEvent(img, 'mouseup', mouseupHandler);
             if(textObject && textObject.content){
                 textObject.content.component.listeners && svg.removeEvent(textObject.content, 'mouseup', textObject.content.component.listeners.mouseup);
