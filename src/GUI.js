cae2f406cc6367fc6b15ef7a3c918e37cb616020
Ealhad <34lh4d@gmail.com>
2016-05-25 11:08:07 +0200

    modified createLink to check if link already exists

--- src/GUI.js
+++ src/GUI.js
@@ -196,17 +196,6 @@ function LibraryDisplay(x,y,w,h){
 
     var displayArrowModeButton = true;
 
-    var createLink = function(parentGame, childGame){
-        parentGame.childrenGames.push(childGame);
-        childGame.parentsGames.push(parentGame);
-        var parentGlobalPoint=parentGame.miniatureManipulator.last.globalPoint(0, parentGame.parentFormation.graphElementSize/2);
-        var parentLocalPoint=parentGame.parentFormation.graphManipulator.last.localPoint(parentGlobalPoint.x, parentGlobalPoint.y);
-        var childGlobalPoint=childGame.miniatureManipulator.last.globalPoint(0, -childGame.parentFormation.graphElementSize/2);
-        var childLocalPoint=parentGame.parentFormation.graphManipulator.last.localPoint(childGlobalPoint.x, childGlobalPoint.y);
-        var arrow = new svg.Arrow(3, 9, 15).position(parentLocalPoint.x,parentLocalPoint.y , childLocalPoint.x, childLocalPoint.y);
-        parentGame.parentFormation.graphManipulator.last.add(arrow);
-    };
-
     for (var i = 0; i<self.tabLib.length; i++) {
         if (i % maxImagesPerLine === 0 && i !== 0) {
             tempY += self.imageHeight + self.libMargin;
@@ -317,6 +306,20 @@ function LibraryDisplay(x,y,w,h){
         var arrowModeManipulator = new Manipulator(self);
         self.libraryManipulator.last.add(arrowModeManipulator.first);
         arrowModeManipulator.first.move(w / 2, (11 / 20) * h);
+
+        var createLink = function(parentGame, childGame){
+            if(parentGame.childrenGames.indexOf(childGame) != -1) return;
+
+            parentGame.childrenGames.push(childGame);
+            childGame.parentsGames.push(parentGame);
+
+            var parentGlobalPoint=parentGame.miniatureManipulator.last.globalPoint(0, parentGame.parentFormation.graphElementSize/2);
+            var parentLocalPoint=parentGame.parentFormation.graphManipulator.last.localPoint(parentGlobalPoint.x, parentGlobalPoint.y);
+            var childGlobalPoint=childGame.miniatureManipulator.last.globalPoint(0, -childGame.parentFormation.graphElementSize/2);
+            var childLocalPoint=parentGame.parentFormation.graphManipulator.last.localPoint(childGlobalPoint.x, childGlobalPoint.y);
+            var arrow = new svg.Arrow(3, 9, 15).position(parentLocalPoint.x,parentLocalPoint.y , childLocalPoint.x, childLocalPoint.y);
+            parentGame.parentFormation.graphManipulator.last.add(arrow);
+        };
 
         var arrowModeButton = displayText('', w - 2 * MARGIN, (6 / 100) * h, myColors.black, myColors.white, null, self.font, arrowModeManipulator);
         var arrow = new svg.Arrow(3, 9, 15).position(-0.3 * w, 0, 0.3 * w, 0);
