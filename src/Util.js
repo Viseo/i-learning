6418959c4868452519ba74210c2c8e1eac43a003
Anacho <qdebesson@orange.fr>
2016-08-02 16:25:37 +0200

    Update Regex

--- src/Util.js
+++ src/Util.js
@@ -1516,7 +1516,7 @@ exports.Util = function (globalVariables) {
      */
     function Bdd() {
         HEADER_SIZE = 0.05;
-        REGEX = /^([A-Za-z0-9.éèêâàîïëôûùö ©,;°?!'"-]){0,150}$/g;
+        REGEX = /^([A-Za-z0-9.éèêâàîïëôûùö ()©,;°?!'"-/]){0,150}$/g;
         FORMATION_TITLE_REGEX = /^([A-Za-z0-9.,;:!?()éèêâàîïëôûùöÉÈÊÂÀÎÏËÔÛÙÖ '-]){0,50}$/g;
         REGEX_ERROR = "Seuls les caractères alphanumériques, avec accent et \"-,',.;?!°© sont permis.";
         REGEX_ERROR_FORMATION = "Le nom de la formation doit être composé de moins de 50 caractères: alphanumériques ou .,;:!?()";
