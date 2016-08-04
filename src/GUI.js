c9895362fc8a4d5ca3e0224803c8ef3efe9b03e8
zlahjouji <zineblahjouji@gmail.com>
2016-08-04 11:02:47 +0200

    little refactor

--- src/GUI.js
+++ src/GUI.js
@@ -929,15 +929,20 @@ exports.GUI = function (globalVariables) {
                         manageDnD(tabElement.miniature.icon.content,tabElement.movingManipulator);
                     };
                     let mouseupHandler = eventUp => {
-                        drawings.piste.last.remove(tabElement.movingManipulator.first);
-                        let target = drawings.background.getTarget(eventUp.pageX, eventUp.pageY);
-                        if (eventDown.pageX === eventUp.pageX && eventDown.pageY === eventUp.pageY) {
+
+                        let clicAction = () => {
                             tabElement.movingManipulator.last.remove(tabElement.miniatureManipulator.first);
                             this.miniaturesManipulator.last.add(tabElement.miniatureManipulator.first);
                             tabElement.miniatureManipulator.first.move(tabElement.miniaturePosition.x, tabElement.miniaturePosition.y);
-                            // svg.addEvent(tabElement.miniature.icon.cadre, 'mousedown', mouseDownAction);
-                            // svg.addEvent(tabElement.miniature.icon.content, 'mousedown', mouseDownAction);
+                            svg.addEvent(tabElement.miniature.icon.cadre, 'mousedown', mouseDownAction);
+                            svg.addEvent(tabElement.miniature.icon.content, 'mousedown', mouseDownAction);
                             tabElement.miniature.miniatureClickHandler();
+                        }
+
+                        drawings.piste.last.remove(tabElement.movingManipulator.first);
+                        let target = drawings.background.getTarget(eventUp.pageX, eventUp.pageY);
+                        if (eventDown.pageX === eventUp.pageX && eventDown.pageY === eventUp.pageY) {
+                            clicAction();
                         }
                         else if (target && target.parent && target.parent.parentManip && (target.parent.parentManip.parentObject instanceof Formation || target.parent.parentManip.parentObject)) {
                             this.dropAction(event, tabElement);
