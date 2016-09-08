7b0a32d168542142a4fb55da6d605e2011079a6c
agcaroff <anne.caroff@telecom-bretagne.eu>
2016-09-08 10:44:32 +0200

    correctif decalage fichier en cours d'upload

--- src/GUI.js
+++ src/GUI.js
@@ -532,6 +532,7 @@ exports.GUI = function (globalVariables) {
                             manipulator.redCrossManipulator.add(redCross);
                             let redCrossClickHandler = ()=> {
                                 dbListener.uploadRequest && dbListener.uploadRequest.abort();
+                                this.videosUploadManipulators.remove(manipulator);
                                 manipulator.flush();
                             };
                             svg.addEvent(redCross, 'click', redCrossClickHandler);
