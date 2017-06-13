4b09bb2d48dd69c837b269e4b7d060499325a603
Thomas Berthaud <thomas.berthaud@viseo.com>
2017-06-13 14:16:12 +0200

    fix formationVue quiz qui disparait

--- src/views/FormationAdminV.js
+++ src/views/FormationAdminV.js
@@ -429,6 +429,7 @@ exports.FormationAdminV = function (globalVariables) {
                     moved: (what) => {
                         let point = what.component.parent.globalPoint(what.x, what.y);
                         this.dropAction(point.x, point.y, game);
+                        this.displayGraph();
                         return true;
                     }
                 };
@@ -575,9 +576,9 @@ exports.FormationAdminV = function (globalVariables) {
             };
 
             let dropLocation = getDropLocation(x, y);
-            let level = getLevel(dropLocation);
-            let column = getColumn(dropLocation, level);
             if (dropLocation.x >= 0) {
+                let level = getLevel(dropLocation);
+                let column = getColumn(dropLocation, level);
                 this.moveGame(item, level, column);
             }
         }
