703eb7c89e7c030bb52ce7b75be1fb957b268fa8
Minh-Huy LE <minhhuylecontact@gmail.com>
2017-06-27 15:06:16 +0200

    -correction dpi formation

--- test/DollCollab.js
+++ test/DollCollab.js
@@ -1,7 +1,7 @@
 
 const assert = require('assert'),
     testutils = require('../lib/testutils'),
-    {given, when, loadPage} = testutils;
+    {given, when, loadPage, clickElement, assertPresent} = testutils;
 
 describe('doll collab', function(){
     it('should display', function(){
@@ -18,5 +18,15 @@ describe('doll collab', function(){
 
         })
     });
+    it('should add a element zone', function(){
+        let {root, type} = given(()=>{
+            let elements = JSON.parse('{"elements" : [ { "type" : "rect", "width" : 406, "height" : 213, "globalX" : 519, "globalY" : 191.2375, "layerIndex" : 0, "fillColor" : [ 25, 122, 230 ], "strokeColor" : [ 0, 0, 0 ] }, { "type" : "text", "width" : 95, "height" : 52, "globalX" : 134.5, "globalY" : 98.7375, "layerIndex" : 1, "fillColor" : [ 255, 255, 255 ], "textMessage" : "dtt" } ]}').elements;
+            return loadPage('GameCollab', {data:{type:'Doll', label:'testDoll', elements:elements, id:'testDollId'}, className:'Doll'});
+        });
+        when(()=>{
+        }).then(()=>{
+            assertPresent(root, 'rectElement0')
+        })
+    })
 })
 
